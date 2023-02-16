07465c1ffaa8336bc55b2d983ec0a1e44ac8cd4a
heitorborbamarini <rm92976@fiap.com.br>
2023-02-16 07:29:41 -0300

    Adicionar arquivos de projeto.

---
+++ byteBank_ADM/Program.cs
@@ -0,0 +1,15 @@
+﻿using byteBank_ADM.Funcionários;
+
+Funcionario Pedro = new Funcionario();
+
+Pedro.Nome = "Pedro Corte";
+
+Pedro.Cpf = "37161659809";
+
+Pedro.Salario = 1750;
+
+Console.WriteLine(Pedro.Nome);
+
+Console.WriteLine(Pedro.GetBonificacao());
+
+
